b4a7a193f0b414cb2ccae9a0ffd0052f9adcf0d9
Falak Mohsin <falak.mohsin18@gmail.com>
2026-07-02 12:21:14 +0530

    javascript

---
+++ javascript/index.js
@@ -0,0 +1,30 @@
+/*let a = 23;
+let b = "name";
+let c = true;
+let d;
+let e = null;
+let f = [1,2,3];
+let g = {name:"Falak", age:21};
+let h = function name(){
+    console.log(typeof(a));
+    console.log(typeof(b));
+    console.log(typeof(c));
+    console.log(typeof(d));
+    console.log(typeof(e));
+    console.log(typeof(f));
+    console.log(typeof(g));
+    console.log(typeof(h));
+};
+h();*/
+
+/*function operation (a,b){
+    return a+b;
+
+}
+let result =  operation(2,3);
+console.log(result);*/
+
+// anonymous function
+let a = function(){
+    
+}

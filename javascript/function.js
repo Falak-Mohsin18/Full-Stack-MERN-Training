da239d4b030536d8033715b271dcaffb16ff1ad5
Falak Mohsin <falak.mohsin18@gmail.com>
2026-07-08 10:55:23 +0530

    javascript

--- javascript/function.js
+++ javascript/function.js
@@ -7,13 +7,33 @@
 // }
 // f1(f2);
 
-function greet(name, callback){
-    console.log(`hi i am ${name}`);
-    callback();
+// function greet(name, callback){
+//     console.log(`hi i am ${name}`);
+//     callback();
+// }
+
+// function happen(){
+//     console.log("Task Completed");
+// }
+
+// greet("Falak", happen)
+
+function checkresult(marks, callback) {
+    if (marks > 40) {
+        console.log("Pass");
+        callback();
+    } else {
+        console.log("Fail");
+    }
 }
 
-function happen(){
-    console.log("Task Completed");
+function happen() {
+    console.log("student passed");
 }
 
-greet("Falak", happen)
+checkresult(50, happen);
+
+function greet(){
+    console.log("I am a Student....");
+}
+setTimeout(greet,5000);

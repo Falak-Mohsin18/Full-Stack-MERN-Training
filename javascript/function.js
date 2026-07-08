bf7870a06dc3b4fa7c2fc5f4ec7f0c01ecf0ee4d
Falak Mohsin <falak.mohsin18@gmail.com>
2026-07-08 11:50:58 +0530

    javascript

--- javascript/function.js
+++ javascript/function.js
@@ -38,16 +38,67 @@
 // }
 // setTimeout(greet,5000);
 
-function login(name) {
-    console.log(`LOGGING IN ${name}`);
+// function login(name) {
+//     console.log(`LOGGING IN ${name}`);
+// }
+
+// function happen() {
+//     console.log("Login Successful");
+// }
+
+// setTimeout(() => {
+//     login("Falak");
+// }, 1000);
+
+// setTimeout(happen, 3000);
+
+// //callback hell( pyramid of doom)
+
+// setTimeout(() =>{
+//     console.log("Step1: task1 completed..");
+//     setTimeout(() =>{
+//         console.log("Step2: Task2 uncomplete..");
+//         setTimeout(() =>{
+//             console.log("Step3 : Task 3 uncomplete..");
+//             setTimeout(()=>{
+//                 console.log("Step4: Task4 completed..");
+//             },4000);
+//         },3000);
+//     },2000);
+// },1000);
+
+/* Student Registration 
+1.resigterStudent
+2.verifyOTP
+3.generateStudentID
+
+using callback hell show student registration complete*/
+
+function registerStudent(name, callback) {
+    setTimeout(() => {
+        console.log(`Student ${name} registration successful`);
+        callback(); 
+    }, 1000);
 }
 
-function happen() {
-    console.log("Login Successful");
+function verifyOTP(callback) {
+    setTimeout(() => {
+        console.log("OTP verified successfully");
+        callback(); 
+    }, 3000);
 }
 
-setTimeout(() => {
-    login("Falak");
-}, 1000);
+function generateStudentID(callback) {
+    setTimeout(() => {
+        console.log("Student ID generated successfully");
+        callback(); 
+    }, 5000);
+}
 
-setTimeout(happen, 3000);
+registerStudent("Falak", () => {
+    verifyOTP(() => {
+        generateStudentID(() => {
+            console.log("Student Registration Complete!");
+        });
+    });
+});
